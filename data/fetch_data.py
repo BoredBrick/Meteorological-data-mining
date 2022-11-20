1cd9bf53d9427add87ec13bee61daf8ffcc6ba50
matusbalazi <balazi@stud.uniza.sk>
2022-11-20 16:37:42 +0100

    Fetching data from other API

---
+++ data/fetch_data.py
@@ -0,0 +1,43 @@
+import datetime as dt
+import requests
+
+# OPEN WEATHER MAP API guide - https://openweathermap.org/current
+
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+#API_KEY = open('api_key', 'r').read()
+API_KEY = "1e71cfe93daced52e4c97934b1f9576b"
+# CITY = "Split"
+
+def kelvin_to_celsius_fahrenheit(kelvin):
+    celsius = kelvin - 273.15
+    fahrenheit = celsius * (9/5) + 32
+    return celsius, fahrenheit
+
+def fetch_data(city = None) -> requests.models.Response:
+    # fetch data
+    url = BASE_URL + "appid=" + API_KEY + "&q=" + city
+    response = requests.get(url).json()
+    return response
+
+def print_data(response = None):
+    city = response['name']
+    temp_kelvin = response['main']['temp']
+    temp_celsius, temp_fahrenheit = kelvin_to_celsius_fahrenheit(temp_kelvin)
+    feels_like_kelvin = response['main']['feels_like']
+    feels_like_celsius, feels_like_fahrenheit = kelvin_to_celsius_fahrenheit(feels_like_kelvin)
+    wind_speed = response['wind']['speed']
+    humidity = response['main']['humidity']
+    description = response['weather'][0]['description']
+    sunrise_time = dt.datetime.utcfromtimestamp(response['sys']['sunrise'] + response['timezone'])
+    sunset_time = dt.datetime.utcfromtimestamp(response['sys']['sunset'] + response['timezone'])
+
+    # print data
+    print(f"Temperature in {city}: {temp_celsius:.2f}°C or {temp_fahrenheit:.2f}°F")
+    print(f"Temperature in {city} feels like: {feels_like_celsius:.2f}°C or {feels_like_fahrenheit:.2f}°F")
+    print(f"Humidity in {city}: {humidity}%")
+    print(f"Wind Speed in {city}: {wind_speed}m/s")
+    print(f"General Weather in {city}: {description}")
+    print(f"Sun rises in {city}: {sunrise_time} local time")
+    print(f"Sun sets in {city}: {sunset_time} local time")
+
+    # print(response)
--- main.py
+++ main.py
@@ -7,26 +7,43 @@ from datetime import date
 import matplotlib.pyplot as plt
 
 from data import process_airmass as airmass
+from data import fetch_data as fetcher
 
 warnings.simplefilter("ignore")
 if __name__ == '__main__':
-    response = airmass.get_airmass_region(45, 11, 52, 23)
 
-    NUM_ROWS = 1
-    IMGs_IN_ROW = 2
-    f, ax = plt.subplots(NUM_ROWS, IMGs_IN_ROW, figsize=(15, 6))
+    print()
+    print("[1] Fetch Airmass Data")
+    print("[2] Fetch Other Data")
+    option = input("Choose an option: ")
 
-    f1 = urllib.request.urlopen(response.url)  # create file-like objects from urls
-    a = plt.imread(f1, 0)  # read the images file in a numpy array
+    if (option == "1"):
+        response = airmass.get_airmass_region(45, 11, 52, 23)
 
-    ax[0].imshow(a)
-    ax[0].set_title('Airmass RGB')
-    title = 'AIRMASS IN CZECHOSLOVAKIA, ', date.today()
-    f.suptitle(title, fontsize=20, y=1.05)
-    plt.tight_layout()
-    plt.show()
+        NUM_ROWS = 1
+        IMGs_IN_ROW = 2
+        f, ax = plt.subplots(NUM_ROWS, IMGs_IN_ROW, figsize=(15, 6))
 
-    folder_name = "AIRMASS//"  # Donwload the files in a folder with the name of the product
-    os.makedirs(folder_name, exist_ok=True)
-    img_landing = folder_name + time.strftime("%Y_%m_%d-%H_%M_%S") + ".jpg"
-    urllib.request.urlretrieve(response.url, img_landing)
+        f1 = urllib.request.urlopen(response.url)  # create file-like objects from urls
+        a = plt.imread(f1, 0)  # read the images file in a numpy array
+
+        # SHOW DOWNLOADED IMAGE (we don't need that now)
+        # ax[0].imshow(a)
+        # ax[0].set_title('Airmass RGB')
+        # title = 'AIRMASS IN CZECHOSLOVAKIA, ', date.today()
+        # f.suptitle(title, fontsize=20, y=1.05)
+        # plt.tight_layout()
+        # plt.show()
+
+        folder_name = "AIRMASS//"  # Donwload the files in a folder with the name of the product
+        os.makedirs(folder_name, exist_ok=True)
+        img_landing = folder_name + time.strftime("%Y_%m_%d-%H_%M_%S") + ".jpg"
+        urllib.request.urlretrieve(response.url, img_landing)
+        print("Image of Airmass was stored succesfully!")
+
+    if (option == "2"):
+        city = input("Choose a city: ")
+        response = fetcher.fetch_data(city)
+        fetcher.print_data(response)
+
+
