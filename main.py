3be48802a85b2496e407ebfe02f42fbc55697d06
Matúš <balazi@stud.uniza.sk>
2023-01-14 10:17:30 +0100

    Endless fetching data for locations and cities

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import argparse
 import sys
 import time
 import warnings
-
+import math
 import keyboard
 from PIL import Image
 
@@ -50,26 +50,59 @@ def main():
                     continue
 
         elif args["fetch"] == "all":
-            city = input("Choose a city: ")
+            print()
+            print("[1] Fetch For Location")
+            print("[2] Fetch For City")
+            print("[0] Quit application")
+            option_location = input("Choose an option: ")
 
+            match option_location:
+                case "1":
+                    print_locations()
+                    location = input("Choose an area: ")
+
+                case "2":
+                    city = input("Choose a city: ")
+
+                case "0":
+                    break
+
+                case _:
+                    continue
+
+            print()
             print("[1] Fetch once")
             print("[2] Endless fetching")
-            option = input("Choose an option: ")
+            option_fetching = input("Choose an option: ")
 
-            data, image = fetcher.fetch_city_data(city)
+            while not keyboard.is_pressed('q'):
 
-            if option == "1":
-                break
+                if option_location == "1":
+                    response = airmass.get_airmass_region(get_val_by_index(int(location)))
+                    image_path = airmass.get_airmass_image(response)
+                    image = Image.open(image_path)  # import to database
+                    #image.show()
+                else:
+                    response = fetcher.fetch_data(city)
+                    fetcher.get_data(response)
+                    data = fetcher.data_to_json(response)  # import to database
+                    coords = fetcher.get_latitude_longitude(response)
 
-            looping = True
-            while looping:
-                data, image = fetcher.fetch_city_data(city)
-                for _ in range(100):
-                    time.sleep(0.1)
-                    if keyboard.is_pressed('q'):
-                        looping = False
-                        break
+                    response = airmass.get_airmass_region((math.floor(coords[0]) - 1, math.floor(coords[1]) - 1,
+                                                           math.ceil(coords[0]) + 1, math.ceil(coords[1]) + 1))
+                    image_path = airmass.get_airmass_image(response)
+                    image = Image.open(image_path)  # import to database
+                    # image.show()
 
+                if option_fetching == "1":
+                    break
+                else:
+                    waiting = 0
+                    while not keyboard.is_pressed('q'):
+                        waiting += 0.1
+                        if waiting >= 10:
+                            break
+                        time.sleep(0.1)
 
 if __name__ == "__main__":
     main()
